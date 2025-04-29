7ead47eb5c60b020fc1a85b77bb07881c2d707b8
Commander-YX <ahmad.hamdan@sandnesskolen.no>
2025-04-29 11:16:53 +0200

    Added a rock-paper-scissors game to the game folder

---
+++ games/guess_the_number.py
@@ -0,0 +1,29 @@
+import random
+guessesTaken = 0
+
+playerName = input("Hellp! What is your name? ")
+
+number = random.randint(1,20)
+print(f"Well, {playerName}, I am thinking of a number between 1 and 20.")
+
+for guessesTaken in range(6):
+    print("Take a guess.")
+    guess = input()
+    guess =int(guess)
+
+    if guess < number:
+        print("Your guess is too low.")
+
+    if guess > number:
+        print("Your guess is too high.")
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken + 1)
+    print(f"Good job, {playerName}! You guessed my number in {guessesTaken} guesses!")
+
+if guess != number:
+    number = str(number)
+    print(f"Sorry, {playerName}! The number I was thinking of was {number}.")
